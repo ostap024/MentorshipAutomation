16e50bbf83ec8a49411e7316f3ad2af4cc3b554e
Ostap Vronskyi <ovron@softserveinc.com>
2019-08-20 12:14:33 +0300

    Add WebDriver

---
+++ MentorshipAutomation.Base/Class1.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Events;
+
+namespace MentorshipAutomation.Base
+{
+    public static class WebDriver
+    {
+        private static IWebDriver _driver;
+
+        public static IWebDriver GetDriver()
+        {
+            return _driver ?? (_driver = new ChromeDriver());
+        }
+
+        public static void Reset()
+        {
+            if (_driver == null) return;
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
--- MentorshipAutomation.Test/UnitTest1.cs
+++ MentorshipAutomation.Test/UnitTest1.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace MentorshipAutomation.Test
 {
     public class Tests
     {
